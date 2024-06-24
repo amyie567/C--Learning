ffda0620453a8cbcc80d80d401ca82264aaf5958
Amit Ranjan <98614510+amyie567@users.noreply.github.com>
2024-06-24 22:10:13 +0530

    Add files via upload

---
+++ c7.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main(){
+
+// int age ;
+char q;
+printf("Are you an indian citizen y/n ");
+scanf("%c",&q);
+// printf("enter the age ");
+// scanf("\n%d",&age);
+
+// if( q=='y'&& age>=18 && age<70 ){
+
+  
+//     printf("you can vote ");
+//   }else{
+//   printf("you cannot vote");
+// }
+
+// ternary operator 
+ q=='y'?printf("vote"):printf("cannot vote");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(q=='y'){
+
+// printf("Enter your age :");
+// scanf("%d",&age);
+
+
+//   if(age>=18){
+//     printf("You can caste vote!!");
+//   }
+//   else if(age>70){
+//     printf("you are a senior citizen");
+//   }
+//   else{
+//     printf("you are less than required age to caste vote!!");
+//   }
+// }
+
+
+
+return 0;
+
+
+}
